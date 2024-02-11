ca2fa41d18a31ef4f15d7fd0460b4e2e8f820350
Ankur Kumar <sandyankurkumar@gmail.com>
2024-02-11 15:16:29 +0530

    GitdataUpdate

--- index.js
+++ index.js
@@ -1,7 +1,41 @@
 const express= require('express')
  require('dotenv').config()
 const app=express()
-
+const githubdata={
+    
+  "login": "hiteshchoudhary",
+  "id": 11613311,
+  "node_id": "MDQ6VXNlcjExNjEzMzEx",
+  "avatar_url": "https://avatars.githubusercontent.com/u/11613311?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/hiteshchoudhary",
+  "html_url": "https://github.com/hiteshchoudhary",
+  "followers_url": "https://api.github.com/users/hiteshchoudhary/followers",
+  "following_url": "https://api.github.com/users/hiteshchoudhary/following{/other_user}",
+  "gists_url": "https://api.github.com/users/hiteshchoudhary/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/hiteshchoudhary/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/hiteshchoudhary/subscriptions",
+  "organizations_url": "https://api.github.com/users/hiteshchoudhary/orgs",
+  "repos_url": "https://api.github.com/users/hiteshchoudhary/repos",
+  "events_url": "https://api.github.com/users/hiteshchoudhary/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/hiteshchoudhary/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Hitesh Choudhary",
+  "company": null,
+  "blog": "https://www.youtube.com/c/HiteshChoudharydotcom",
+  "location": "India",
+  "email": null,
+  "hireable": null,
+  "bio": "I make coding videos on youtube and for courses. My youtube channel explains my work more. Check that out",
+  "twitter_username": "hiteshdotcom",
+  "public_repos": 81,
+  "public_gists": 3,
+  "followers": 15076,
+  "following": 0,
+  "created_at": "2015-03-23T13:03:25Z",
+  "updated_at": "2023-12-31T10:07:11Z"
+}
 app.get('/',(req,res)=>{
     res.send("Hello World !")
 })
@@ -14,6 +48,9 @@ app.get('/login',(req,res)=>{
 })
 app.get('/youtube',(req,res)=>{
     res.send("HEllo welcome to chai and sutta");
+})
+app.get('/github',(req,res)=>{
+    res.json(githubdata)
 })
 app.listen(process.env.PORT,()=>{
     console.log(`Port is listing ${process.env.PORT}`)
